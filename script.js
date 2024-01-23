fd334395821b69d48d53fef84409e8f21b50d593
AliKhachab <a.khachab620@gmail.com>
2024-01-23 01:19:25 -0500

    finalized project + added imaginary 'buy pro' feature

--- script.js
+++ script.js
@@ -5,31 +5,40 @@ const counterValueEl = document.querySelector(".counter__value");
 const counterEl = document.querySelector(".counter");
 const counterTitleEl = document.querySelector(".counter__title");
 
-function increaseCounter() {
+const increaseCounter = () =>  {
     const currentValue = counterValueEl.textContent;
     const currentValueAsNumber = +currentValue;
     let newValue = currentValueAsNumber + 1;
     if (newValue > 5) {
         newValue = 5;
         counterEl.classList.add("counter--limit");
-        counterTitleEl.innerHTML = "Limit reached! Buy <b>PRO</b> for more.";
+        counterTitleEl.innerHTML = 'Limit! Buy <b>Pro</b> for >5';
+        increaseButtonEl.disabled = true;
+        decreaseButtonEl.disabled = true;
+        increaseButtonEl.blur();
     }
     counterValueEl.textContent = newValue;
 }
 
 increaseButtonEl.addEventListener("click", increaseCounter);
 
-decreaseButtonEl.addEventListener("click", function() {
+decreaseButtonEl.addEventListener("click", () => {
     counterEl.classList.remove("counter--limit");
     const currentValue = counterValueEl.textContent;
     const currentValueAsNumber = +currentValue;
     let newValue = currentValueAsNumber - 1;
     if (newValue < 0) newValue = 0;
     counterValueEl.textContent = newValue;
+    decreaseButtonEl.blur();
 });
 
-resetButtonEl.addEventListener("click", function() {
+resetButtonEl.addEventListener("click", () => {
     counterValueEl.textContent = 0;
+    counterEl.classList.remove("counter--limit");
+    counterTitleEl.innerHTML = "Fancy Counter";
+    increaseButtonEl.disabled = false;
+    decreaseButtonEl.disabled = false;
+    resetButtonEl.blur();
 });
 
 document.addEventListener("keydown", increaseCounter);
